bc2c50557d2922134f8cfded0060366a5260d7f5
Toni <aiming1high@gmail.com>
2022-10-12 03:33:01 +0300

    5.18 is done

---
+++ cypress/e2e/blog_app.cy.js
@@ -0,0 +1,42 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+
+  it('Login form is shown', function () {
+    cy.contains('login')
+    cy.get('#username')
+    cy.get('#password')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged-in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
+  })
+})
+
